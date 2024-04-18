9e8fcd207a13ace4c9b2b9f66aea3b60b258d8fe
Raviteja Gandam <gandamraviteja@gmail.com>
2024-04-18 15:48:55 +0530

    Completed UI elements for team page

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import './App.css';
+import MatchPage from './pages/MatchPage';
 import TeamPage from './pages/TeamPage';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, Switch } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
       <Router>
+
         <Routes>
           <Route path="/teams/:teamName" element={<TeamPage />}></Route>
+          <Route path="/teams/:teamName/matches/:year" element={<MatchPage />}></Route>
+          {/* <Route path="/teams/:teamName" element={<TeamPage />}></Route> */}
         </Routes>
       </Router>
     </div>
---
+++ src/pages/MatchPage.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function MatchPage() {
+  return (
+    <div>
+      <h1>Match page</h1>
+    </div>
+  );
+}
+
+export default MatchPage;
